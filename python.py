96ef5e62babc1d4e9f4283c9000f45289e39f580
AkhilReddy0409 <58068731+AkhilReddy0409@users.noreply.github.com>
2020-04-11 14:08:29 +0530

    complete family tree
    complete code

--- python.py
+++ python.py
@@ -14,4 +14,81 @@ def sibling(x, y):
 def uncle(x, y):
   temp = var()
    return conde((father(temp, x), grandparent(temp, y)))
-  
+if __name__=='__main__': 
+    father = Relation() 
+    mother = Relation() 
+#Load the data from the relationships.json file:
+
+    with open('relationships.json') as f: 
+        d = json.loads(f.read()) 
+Read the data and add them to our fact base:
+
+    for item in d['father']: 
+        facts(father, (list(item.keys())[0], list(item.values())[0])) 
+ 
+    for item in d['mother']: 
+        facts(mother, (list(item.keys())[0], list(item.values())[0])) 
+
+    x = var() 
+
+    # John's children 
+    name = 'John' 
+    output = run(0, x, father(name, x)) 
+    print("\nList of " + name + "'s children:") 
+    for item in output: 
+        print(item) 
+
+
+    # William's mother 
+    name = 'William' 
+    output = run(0, x, mother(x, name))[0] 
+    print("\n" + name + "'s mother:\n" + output) 
+
+    # Adam's parents  
+    name = 'Adam' 
+    output = run(0, x, parent(x, name)) 
+    print("\nList of " + name + "'s parents:") 
+    for item in output: 
+        print(item) 
+
+    # Wayne's grandparents  
+    name = 'Wayne' 
+    output = run(0, x, grandparent(x, name)) 
+    print("\nList of " + name + "'s grandparents:") 
+    for item in output: 
+        print(item) 
+
+
+    # Megan's grandchildren  
+    name = 'Megan' 
+    output = run(0, x, grandparent(name, x)) 
+    print("\nList of " + name + "'s grandchildren:") 
+    for item in output: 
+        print(item) 
+
+
+    # David's siblings  
+    name = 'David' 
+    output = run(0, x, sibling(x, name)) 
+    siblings = [x for x in output if x != name] 
+    print("\nList of " + name + "'s siblings:") 
+    for item in siblings: 
+        print(item) 
+
+
+    # Tiffany's uncles 
+    name = 'Tiffany' 
+    name_father = run(0, x, father(x, name))[0] 
+    output = run(0, x, uncle(x, name)) 
+    output = [x for x in output if x != name_father] 
+    print("\nList of " + name + "'s uncles:") 
+    for item in output: 
+        print(item) 
+
+
+    # All spouses 
+    a, b, c = var(), var(), var() 
+    output = run(0, (a, b), (father, a, c), (mother, b, c)) 
+    print("\nList of all spouses:") 
+    for item in output: 
+        print('Husband:', item[0], '<==> Wife:', item[1])  
